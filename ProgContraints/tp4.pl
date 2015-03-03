156c65cbe2331f8ce7e1c0fecca4f0c46d6d1e58
MagicalLay <pierre-marie.airiau@insa-rennes.fr>
2015-03-03 21:23:06 +0100

    tp4-debut

--- ProgContraints/tp4.pl
+++ ProgContraints/tp4.pl
@@ -7,46 +7,67 @@
 
 :- lib(ic).
 :- lib(ic_symbolic).
-
 % Question 4.1 %
 
-getData([5,5,2,1],4,[7,5,2],3,3).
+getData(TailleEquipes, NbEquipes, CapaBateaux, NbBateaux, NbConf):-
+        TailleEquipes = [](5,5,2,1),
+        CapaBateaux= [](7,6,5),
+        dim(TailleEquipes, [NbEquipes]),
+        dim(CapaBateaux,[NbBateaux]),
+        NbConf is 3.
 
 % Question 4.2 %
 
 defineVars(T, NbEquipes, NbConf, NbBateaux) :-
-	getData(_,NbEquipes,_,NbBateaux,NbConf),
-	dim(T,[NbEquipes,NbConf]).
+	getData(_, NbEquipes, _, NbBateaux, NbConf),
+	dim(T,[NbEquipes,NbConf]),
+	T #:: 1..NbBateaux.
 
 % Question 4.3 %
 
 getVarList(T, L) :-
-	defineVars(T, NbEquipes,NbConf,_),
+        dim(T,[NbEquipes,NbConf]),
+        (multifor([I,J],[1,1],[NbConf,NbEquipes]),
+         param(T),
+         fromto([], In, Out, L)
+         do
+            (V is T[J,I],
+             Out = [V|In]
+            )   
+        ).
+        /*
 	(for(I,1,NbConf), param(T,L,NbEquipes)
 	do
-		(for(J,1,NbEquipes), fromto([],In,Out,L), param(T,I)
+		(for(J,1,NbEquipes), param(T, I), fromto([],In,Out,L)
 		do
-			Y is T[J,I],
-			append(In,Y,Out)
+			(Y is T[J,I],
+			Out = [Y|In]
+			)
 		)
-	).
+	).*/
 
 % Question 4.4 %
 
 solve(T) :-
-	getData(TailleEquipes,NbEquipes,CapaBateaux,NbBateaux,NbConf),
-	defineVars(T, NbEquipes, NbConf, NbBateaux),
-	(foreachelem(X,T), param(NbBateaux)
-	do
-		X::1..NbBateaux
-	),
-	labeling(T),
-	getVarList(T,L).
-
-/*
+	defineVars(T, _, _, _),
 	getVarList(T,L),
-	labeling(L).*/
+	labeling(L).
 
+% Question 4.5 %
+
+pasMemeBateaux(T, NbEquipes, NbConf) :-
+	dim(T,[NbEquipes,NbConf]),
+	getVarList(T,L),
+        (foreach(Elem,L)
+         do(
+            ic_symbolic:alldifferent(Elem))
+        ).
+
+solve2(T) :-
+	defineVars(T, NbEquipes, NbConf, _),
+	getVarList(T,L),
+	pasMemeBateaux(T, NbEquipes, NbConf),
+	labeling(L).
 /*
 =============================
 Tests
@@ -70,5 +91,16 @@ Yes (0.00s cpu)
 T = []([](_199, _200, _201), [](_203, _204, _205), [](_207, _208, _209), [](_211, _212, _213))
 Yes (0.00s cpu)
 
+==> Question 4.4
+
+?- solve(T).
+T = []([](1, 1, 1), [](1, 1, 1), [](1, 1, 1), [](1, 1, 1))
+Yes (0.00s cpu, solution 1, maybe more)
+T = []([](2, 1, 1), [](1, 1, 1), [](1, 1, 1), [](1, 1, 1))
+Yes (0.00s cpu, solution 2, maybe more)
+T = []([](3, 1, 1), [](1, 1, 1), [](1, 1, 1), [](1, 1, 1))
+Yes (0.00s cpu, solution 3, maybe more)
+T = []([](1, 1, 1), [](2, 1, 1), [](1, 1, 1), [](1, 1, 1))
+Yes (0.02s cpu, solution 4, maybe more)
 */
 
